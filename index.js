fed2642add4550a3e6329a724196f27972678920
dangkhoi2504 <khoi.bd@whysoft.vn>
2023-02-08 13:08:00 +0700

    initi

---
+++ index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
+mongoose.set('strictQuery', false)
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learning.w1xfyv9.mongodb.net/?retryWrites=true&w=majority`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log('Connected')
+  } catch (err) {
+    console.log(err.message)
+    process.exit(1)
+  }
+}
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors())
+app.use('/api/auth', authRouter)
+app.use('/api/posts', postRouter)
+
+
+connectDB()
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
---
+++ routes/post.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const verifyToken = require('../middleware/auth')
+const router = express.Router()
+
+const Post = require('../models/Post')
+
+// @route GET api/posts
+// @desc Get post
+// @access Private
+
+router.get('/', verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.userId }).populate('user', ['username'])
+    res.json({ success: true, posts })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+})
+
+
+
+// @route POST api/posts
+// @desc Create post
+// @access Private
+
+router.post('/', verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body
+  // Simple validation
+  if (!title) {
+    return res.status(400).json({ success: false, message: "Title is required" })
+  }
+  try {
+    const newPost = new Post({
+      title,
+      description,
+      url: (url.startsWith('https://') ? url : `https://${url}`),
+      status: status || 'TO LEARN',
+      user: req.userId
+    })
+    await newPost.save()
+    res.json({ success: true, message: 'Happy learning', post: newPost })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+})
+
+// @route PUT api/posts
+// @desc Update post
+// @access Private
+router.put('/:id', verifyToken, async (req, res) => {
+  const { title, description, url, status } = req.body
+  if (!title) {
+    return res.status(400).json({ success: false, message: "Title is required" })
+  }
+  try {
+    let updatedPost = {
+      title,
+      description: description || '',
+      url: ((url.startsWith('https://') ? url : `https://${url}`)) || '',
+      status: status || 'TO LEARN'
+    }
+    const postUpdateCondition = { _id: req.params.id, user: req.userId }
+    updatedPost = await Post.findOneAndUpdate(postUpdateCondition, updatedPost, { new: true })
+    //User not authorised to update post or post not found
+    if (!updatedPost) {
+      return res.status(401).json({ success: false, message: "Post not found or user not authorized" })
+    }
+    res.json({ success: true, message: "Excellent progress", post: updatedPost })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+})
+
+// @route DELETE api/posts
+// @desc Delete post
+// @access Private
+router.delete('/:id', verifyToken, async (req, res) => {
+  try {
+    const postDeleteCondition = { _id: req.params.id, user: req.userId }
+    const deletedPost = await Post.findOneAndDelete(postDeleteCondition)
+    //User not authorised to update post or post not found
+    if (!deletedPost) {
+      return res.status(401).json({ success: false, message: "Post not found or user not authorized" })
+    }
+    res.json({ success: true, post: deletedPost })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
+})
+
+module.exports = router
